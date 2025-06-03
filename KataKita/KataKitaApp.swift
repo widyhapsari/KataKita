b1796a5873a783212aa5733a3fe3a0a278f4e41c
widyhapsari <rastyawidya@gmail.com>
2025-06-03 11:06:28 +0700

    Initial Commit

---
+++ KataKita/KataKitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KataKitaApp.swift
+//  KataKita
+//
+//  Created by Rastya Widya Hapsari on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct KataKitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KataKita/VoiceInput.swift
@@ -0,0 +1,9 @@
+//
+//  VoiceInput.swift
+//  KataKita
+//
+//  Created by Rastya Widya Hapsari on 03/06/25.
+//
+
+import Speech
+import AVFoundation
